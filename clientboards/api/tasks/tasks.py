197de812039b9969f481caddaef1f0b281ca1253
diontugit <diontugit@gmail.com>
2024-06-23 16:25:54 -0400

    minor: added tasks and initiated a task for testing purposes

--- clientboards/api/tasks/tasks.py
+++ clientboards/api/tasks/tasks.py
@@ -4,3 +4,7 @@ from celery import shared_task
 @shared_task
 def log(message):
     print(message)
+
+@shared_task
+def add(one, two):
+    return one + two
--- clientboards/api/views/users/users_view.py
+++ clientboards/api/views/users/users_view.py
@@ -6,7 +6,14 @@ from clientboards.api.response.response import ResponseGenerator
 # services
 from clientboards.api.services.users.users_services import UsersServices
 
+# tasks
+from clientboards.api.tasks.tasks import log, add
+
 
 class UsersAPIView(APIView):
     def get(self, request):
+        # TODO: remove below code. it is just for testing tasks
+        result = add.delay(1, 2)
+        log.delay('logger: nice')
+        print('task nice:', result.id)
         return ResponseGenerator(lambda: UsersServices.getUsers())
