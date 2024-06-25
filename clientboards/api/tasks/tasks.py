760e4e5aa258391689870a307663ec8de48232c0
diontugit <diontugit@gmail.com>
2024-06-25 16:50:50 -0400

    minor: create the task to save the blocks

--- clientboards/api/tasks/tasks.py
+++ clientboards/api/tasks/tasks.py
@@ -5,6 +5,13 @@ from celery import shared_task
 def log(message):
     print(message)
 
+
 @shared_task
 def add(one, two):
     return one + two
+
+
+@shared_task
+def saveBlock(id: int, properties: dict, content: str):
+    # TODO: save the block using the id, properties, and content
+    pass
