b1f15d3e7ac6cca49151d76029a4e7a98de2d82f
diontugit <diontugit@gmail.com>
2024-07-02 08:56:26 -0400

    minor: create method to set default block permissions

---
+++ clientboards/api/services/block_permissions/block_permissions_services.py
@@ -0,0 +1,9 @@
+from clientboards.api.models.blocks.models import Blocks
+
+
+class BlockPermissionsServices:
+    @staticmethod
+    def setDefaultPermissions(block: Blocks):
+        # TODO: check if the block exists, if not raise a services error
+        # TODO: set the default permissions
+        pass
